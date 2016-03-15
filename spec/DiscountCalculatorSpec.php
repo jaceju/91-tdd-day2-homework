c68385cf8c119397c110477880c92fb548ff68d5
Jace Ju <jaceju@gmail.com>
2016-03-15 15:08:28 +0800

    加入第四個案例：一二三四集各買了一本，價格應為 (100 * 4 * 0.8) = 320

--- spec/DiscountCalculatorSpec.php
+++ spec/DiscountCalculatorSpec.php
@@ -37,4 +37,14 @@ class DiscountCalculatorSpec extends ObjectBehavior
         $this->calculateDiscount();
         $this->getTotal()->shouldBe(270);
     }
+
+    function it_add_four_different_books_and_sum_should_be_320()
+    {
+        $this->addBook(new Book('哈利波特：神秘的魔法石', 100));
+        $this->addBook(new Book('哈利波特：消失的密室', 100));
+        $this->addBook(new Book('哈利波特：阿茲卡班的逃犯', 100));
+        $this->addBook(new Book('哈利波特：火盃的考驗', 100));
+        $this->calculateDiscount();
+        $this->getTotal()->shouldBe(320);
+    }
 }
